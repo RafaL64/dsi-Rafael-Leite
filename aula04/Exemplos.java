1afbe276629696351cd2fa03dd80953445777901
Rafael Leite <rafaelleitedasilvafrancisco@gmail.com>
2026-04-13 17:25:09 -0300

    lição bacana de variaveis

---
+++ aula04/Exemplos.java
@@ -0,0 +1,17 @@
+package aula04;
+
+public class Exemplos {
+    public static void main(String[] args) {
+        int n1 = 10; // variavel inteira 1
+        int n2 = 67;// variavel inteira 2
+        float decimal = 11.67f; // variavel float
+        double paia = 18.6767; // variavel double
+        double resultado;// soma de todas as veriáveis
+        resultado = n1 + n2 + decimal + paia;
+        char caractere = 'a';
+        String palavra = "receb";
+        System.out.println("A soma de todas as variavei é: " + resultado); // mostra a soma de todas as varivaeis
+        System.out.println(palavra + caractere + "!"); // escreve receba
+
+    }
+}
