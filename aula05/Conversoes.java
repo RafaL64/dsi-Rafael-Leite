bd65627b8d658e5a278a3c9090d980da25121586
Rafael Leite <rafaelleitedasilvafrancisco@gmail.com>
2026-04-27 09:21:26 -0300

    Lição de conversões e uso de operações. O Fessor deixou lição de casa em

---
+++ aula05/Conversoes.java
@@ -0,0 +1,8 @@
+
+public class conversoes {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+        
+        System.out.println(myDouble);
+    }}
---
+++ aula05/Uso.java
@@ -0,0 +1,18 @@
+
+public class Uso {
+
+    public static void main(String[] args) {
+
+        
+    int x  = 9;
+    int y = 3;
+
+    System.out.println(x + y);
+    System.out.println("\n" + (x - y));
+    System.out.println("\n"+x * y);
+    System.out.println("\n"+ x / y);
+    System.out.println("\n" + x++);
+    System.out.println("\n" + y--);
+    System.out.println("\n"+ x % y);
+
+}}
