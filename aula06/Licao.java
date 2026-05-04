d8ac923d3e47308c4301dfb07a4441b6a0f19625
Rafael Leite <rafaelleitedasilvafrancisco@gmail.com>
2026-05-04 09:25:57 -0300

    Lição de comparadores

---
+++ aula06/Licao.java
@@ -0,0 +1,15 @@
+package aula06;
+
+public class Licao {
+    boolean aura = true;
+    boolean alfa = true;
+    boolean mogged = false;
+
+    if(aura&&alfa)
+    {
+        System.out.println("receba");
+    }else
+    {
+        System.out.println("voce é beta");
+    }
+}
